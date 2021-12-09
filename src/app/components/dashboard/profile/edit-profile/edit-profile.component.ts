f5528489a0b92f44b5322b877fb8103f7b3fd102
AntonioFerrandiz <antoniojfb01@gmail.com>
2021-12-09 00:09:51 -0500

    adding new styles

--- src/app/components/dashboard/profile/edit-profile/edit-profile.component.ts
+++ src/app/components/dashboard/profile/edit-profile/edit-profile.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-profile.component.css']
 })
 export class EditProfileComponent implements OnInit {
-
+  disabled = false;
   constructor() { }
 
   ngOnInit(): void {
   }
+  tabLoadTimes: Date[] = [];
 
+  getTimeLoaded(index: number) {
+    if (!this.tabLoadTimes[index]) {
+      this.tabLoadTimes[index] = new Date();
+    }
+
+    return this.tabLoadTimes[index];
+  }
 }
