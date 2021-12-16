11e6ff54afe5549fe5241880e814b0b61495bed9
AntonioFerrandiz <antoniojfb01@gmail.com>
2021-12-16 18:57:06 -0500

    placeholder changes

--- src/app/components/dashboard/profile/edit-profile/edit-profile.component.ts
+++ src/app/components/dashboard/profile/edit-profile/edit-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-edit-profile',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProfileComponent implements OnInit {
   disabled = false;
-  constructor() { }
+  firstname:  string;
+  lastname: string;
+  username: string
+  identityDocument: string;
+  biography: string;
+  constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {
+    this.getUserInfo();
   }
   tabLoadTimes: Date[] = [];
 
@@ -17,7 +24,14 @@ export class EditProfileComponent implements OnInit {
     if (!this.tabLoadTimes[index]) {
       this.tabLoadTimes[index] = new Date();
     }
-
     return this.tabLoadTimes[index];
+  }
+  getUserInfo(): void{
+    this.loginService.getUserData().subscribe(data =>{
+      this.firstname = data["firstname"];
+      this.lastname = data["lastname"];
+      this.username = data["username"];
+      this.identityDocument = data["identityDocument"]
+    })
   }
 }
