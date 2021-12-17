5651be70b42ca8aca17e4cea0daf6886e433e98c
AntonioFerrandiz <antoniojfb01@gmail.com>
2021-12-16 19:06:09 -0500

    adding service

--- src/app/components/dashboard/profile/profile.component.ts
+++ src/app/components/dashboard/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-profile',
@@ -8,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class ProfileComponent implements OnInit {
   showEditProfile = false;
   showOverview = false;
-  constructor() { }
+  firstname: string;
+  lastname: string;
+  constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {
+    this.getUserInfo();
+  }
+  getUserInfo(): void{
+    this.loginService.getUserData().subscribe(data =>{
+      this.firstname = data["firstname"];
+      this.lastname = data["lastname"]
+    })
   }
 
 }
