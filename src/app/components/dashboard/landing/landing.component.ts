c8b49c25ee6df7ec2e15c1cdb0baf721eedd25d1
AntonioFerrandiz <antoniojfb01@gmail.com>
2021-12-21 11:50:17 -0500

    chart

--- src/app/components/dashboard/landing/landing.component.ts
+++ src/app/components/dashboard/landing/landing.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
 import { PatientService } from 'src/app/services/patient.service';
 
+import DatalabelsPlugin from 'chartjs-plugin-datalabels';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import { Subject, Subscription } from 'rxjs';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
@@ -10,29 +15,90 @@ import { PatientService } from 'src/app/services/patient.service';
 export class LandingComponent implements OnInit {
   firstname: string;
   totalPatients: number;
+  patientsMale: number;
+  patientsFemale: number;
   gendersList: any = [];
+  myAppUrl: string;
+  myApiUrl: string;
+
   constructor(private loginService: LoginService,
-              private patientService: PatientService) { }
+    private patientService: PatientService) {
+    this.myAppUrl = environment.endpoint;
+    this.myApiUrl = '/api/Patient';
+  }
 
   ngOnInit(): void {
     this.getUserInfo()
     this.getTotalPatients();
     this.getListOfGenders();
+    this.getTotalPatientsMale()
+
   }
-  getUserInfo(): void{
-    this.loginService.getUserData().subscribe(data =>{
+
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+
+  getUserInfo(): void {
+    this.loginService.getUserData().subscribe(data => {
       this.firstname = data["firstname"];
     })
   }
-  getTotalPatients():void{
-    this.patientService.GetNumberOfPatients().subscribe(data =>{
+  getTotalPatients(): void {
+    this.patientService.GetNumberOfPatients().subscribe(data => {
       this.totalPatients = data;
     })
   }
-  getListOfGenders(): void{
+  getListOfGenders(): void {
     this.patientService.GetListOfGenders().subscribe(data => {
       this.gendersList = data;
-      console.log(this.gendersList)
+      this.patientsFemale = parseInt(this.gendersList[0])
+
     })
   }
+  // async getTotalPatientsMale(): Promise<number> {
+  //   const body = await window.fetch(this.myAppUrl + this.myApiUrl + '/GetListOfGenders')
+  //   const data = await body.json();
+  //   console.log(parseInt(data[1]))
+  //   this.patientsMale = parseInt(data[1]);
+  //   return parseInt(data[1])
+  // }
+
+  getTotalPatientsMale(): number{
+    var value:number;
+    this.patientService.GetListOfGenders().subscribe( async data =>{
+      await this.getCoca(data);
+    })
+    return value;
+  }
+  getCoca(num): number{
+    this.patientsMale = num
+    console.log('x',this.patientsMale);
+    return this.patientsMale
+  }
+
+  // Pie
+  public pieChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top',
+      },
+      datalabels: {
+        formatter: (value, ctx) => {
+          if (ctx.chart.data.labels) {
+            return ctx.chart.data.labels[ctx.dataIndex];
+          }
+        },
+      },
+    }
+  };
+  public pieChartData: ChartData<'pie', number[], string | string[]> = {
+    labels: [['Female'], ['Male']],
+    datasets: [{
+      data: [4, 5]
+    }]
+  };
+  public pieChartType: ChartType = 'pie';
+  public pieChartPlugins = [DatalabelsPlugin];
 }
