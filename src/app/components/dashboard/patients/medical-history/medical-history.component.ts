7ebf305d3b8a218f289192e2a6d6df646df9d691
AntonioFerrandiz <antoniojfb01@gmail.com>
2021-12-27 15:01:05 -0500

    medical history table

--- src/app/components/dashboard/patients/medical-history/medical-history.component.ts
+++ src/app/components/dashboard/patients/medical-history/medical-history.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { PatientService } from 'src/app/services/patient.service';
 import { MatDialogModule, MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
 import { MedicalHistoryFormComponent } from './medical-history-form/medical-history-form.component';
 import { MedicalHistoryService } from 'src/app/services/medical-history.service';
-
+import { MatTableDataSource } from '@angular/material/table';
+import { MedicalHistory } from 'src/app/models/medicalHistory.model';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import {MatSort, Sort} from '@angular/material/sort'
 @Component({
   selector: 'app-medical-history',
   templateUrl: './medical-history.component.html',
@@ -14,15 +17,20 @@ import { MedicalHistoryService } from 'src/app/services/medical-history.service'
 export class MedicalHistoryComponent implements OnInit {
   patientID: number;
   patientData: any = [];
+  medicalHistoryPatient: any = [];
+  displayedColumns = ['age', 'timePeriod', 'heigth', 'weigth', 'details', 'prescribedMedication', 'dateCreated']
+  dataSource = new MatTableDataSource<MedicalHistory>();
   constructor(private patientService: PatientService,
     private medicalHistoryService: MedicalHistoryService,
-    private aRoute: ActivatedRoute, public dialog: MatDialog) {
+    private aRoute: ActivatedRoute, public dialog: MatDialog,
+    private _liveAnnouncer: LiveAnnouncer) {
     this.patientID = +this.aRoute.snapshot.paramMap.get("id");
   }
-
+  @ViewChild(MatSort) sort: MatSort;
   ngOnInit(): void {
     this.getPatient();
     this.getMedicalHistory();
+    this.dataSource.sort = this.sort;
   }
 
   openDialog() {
@@ -49,7 +57,19 @@ export class MedicalHistoryComponent implements OnInit {
 
   getMedicalHistory(): void {
     this.medicalHistoryService.getMedicalHistory(this.patientID).subscribe(data => {
-      console.log(data);
+      this.dataSource = data;
+      console.log(this.dataSource)
     })
+  }
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 }
