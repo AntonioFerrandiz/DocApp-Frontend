f42e862851a70312fbccd19e7d59858e8d096658
AntonioFerrandiz <antoniojfb01@gmail.com>
2021-12-30 16:21:24 -0500

    adding profile picture

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
+import { UserProfilePictureService } from 'src/app/services/user-profile-picture.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,10 +13,11 @@ export class DashboardComponent implements OnInit {
   mobileQuery: MediaQueryList;
   firstname: string;
   lastname: string;
+  profilePictureURL: string;
   private _mobileQueryListener: () => void;
   constructor(changeDetectorRef: ChangeDetectorRef,
     media: MediaMatcher,
-    private loginService: LoginService) {
+    private loginService: LoginService, private userProfilePictureService: UserProfilePictureService) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
@@ -23,6 +25,7 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.getUserInfo();
+    this.getUserProfilePicture()
   }
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
@@ -34,5 +37,10 @@ export class DashboardComponent implements OnInit {
       this.lastname = data["lastname"]
     })
   }
-
+  getUserProfilePicture(): void{
+    this.userProfilePictureService.getProfileImageURL().subscribe(data => {
+      this.profilePictureURL = data[data.length - 1]['profilePictureURL']
+      console.log(this.profilePictureURL)
+    })
+  }
 }
